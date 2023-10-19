1854c03b8a056e25eeccd054c07865df75ac5cca
Miguel Nievas <miguel.nievas.fuertes@gmail.com>
2023-10-19 11:23:19 +0200

    W5 D4 sol

---
+++ 2-Data_Analysis/3-Sources/Archivos/Practica/variables.py
@@ -0,0 +1,6 @@
+ruta = "D:\\Carpeta_profesor\\2309_dsft_thebridge\\2-Data_Analysis\\3-Sources\\Archivos\\Practica\\carpeta_practica"
+
+doc_types = ('.doc', '.docx', '.txt', '.pdf', '.xls', '.ppt', '.xlsx', '.pptx')
+img_types = ('.jpg', '.jpeg', '.png', '.svg', '.gif', '.tif', '.tiff')
+software_types = ('.exe', '.py','.ipynb', '.executable')
+
