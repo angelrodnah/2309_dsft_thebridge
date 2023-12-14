5b88ddc6ad8ab433d695448376280d275c1f3c43
Miguel Nievas <miguel.nievas.fuertes@gmail.com>
2023-12-14 14:25:31 +0100

    W13 D4 class

--- 4-Data_Engineering/1-APIs/BBDD/ejercicio/app_bd.py
+++ 4-Data_Engineering/1-APIs/BBDD/ejercicio/app_bd.py
@@ -1,6 +1,6 @@
 # import json
 from flask import Flask, request, jsonify
-# import sqlite3
+import sqlite3
 import os
 
 os.chdir(os.path.dirname(__file__))
@@ -13,14 +13,73 @@ def welcome():
     return "Welcome to mi API conected to my books database"
 
 # 0.Ruta para obtener todos los libros
-
+@app.route('/v1/resources/books')
+def get_all():
+    connection = sqlite3.connect('books.db')
+    cursor = connection.cursor()
+    query = "SELECT * FROM books"
+    result = cursor.execute(query).fetchall()
+    connection.close()
+    return jsonify(result)
 
 # 1.Ruta para obtener el conteo de libros por autor ordenados de forma descendente
-
+@app.route('/v1/resources/booksbyauthor')
+def get_all_author():
+    connection = sqlite3.connect('books.db')
+    cursor = connection.cursor()
+    query = '''SELECT 
+                author,
+                COUNT(*)
+               FROM books
+               GROUP BY 1
+               ORDER BY 2 DESC'''
+    result = cursor.execute(query).fetchall()
+    connection.close()
+    return jsonify(result)
 
 # 2.Ruta para obtener los libros de un autor como argumento en la llamada
+@app.route('/v1/resources/books/author')
+def book_author():
+    if 'author' not in request.args:
+        return "Author argument missing", 400
+    else:
+        try:
+            author = request.args['author'] # Asimov OR 1=1
+            connection = sqlite3.connect('books.db')
+            cursor = connection.cursor()
+            query = '''SELECT 
+                        *
+                    FROM books
+                    WHERE author LIKE ?'''
+            result = cursor.execute(query, ("%" + author + "%",)).fetchall()
+            connection.close()
+            return jsonify(result), 200
+        except Exception as e:
+            return jsonify({"error":str(e)}), 500
 
 
 # 3.Ruta para obtener los libros filtrados por título, publicación y autor
+@app.route('/v1/resources/books/filters')
+def book_filters():
+    if 'author' not in request.args and 'published' not in request.args and 'title' not in request.args:
+        return "Filters missing", 400
+    else:
+        try:
+            author = request.args['author'] # Asimov OR 1=1
+            published = request.args['published']
+            title = request.args['title']
+            connection = sqlite3.connect('books.db')
+            cursor = connection.cursor()
+            query = '''SELECT 
+                        *
+                    FROM books
+                    WHERE author LIKE ?
+                          AND title LIKE ?
+                          AND published = ?'''
+            result = cursor.execute(query, ("%" + author + "%","%" + title + "%", published)).fetchall()
+            connection.close()
+            return jsonify(result), 200
+        except Exception as e:
+            return jsonify(result), 500
 
 app.run()
---
+++ 4-Data_Engineering/1-APIs/Model/ejercicio/app.py
@@ -0,0 +1,64 @@
+from flask import Flask, request, render_template
+import sqlite3
+import pickle
+import pandas as pd
+
+app = Flask(__name__)
+
+with open('data/new_advertising_model.pkl', 'rb') as file:
+    model = pickle.load(file)
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/ingest', methods=['POST'])
+def ingest():
+    data = request.get_json()
+    new_data = data.get('data', None)
+    if not new_data:
+        return {"error":"No se proporcionaron datso para agregar a la bbdd"}, 400
+
+    try:
+        connection = sqlite3.connect('data/advertising.db')
+        cursor = connection.cursor()
+        query = "INSERT INTO campañas VALUES (?,?,?,?)"
+        for valor in new_data:
+            cursor.execute(query, valor)
+        connection.commit()
+        connection.close()
+        return {'message': 'Datos ingresados correctamente'}, 200
+    except Exception as e:
+        return {'error':str(e)}, 500
+    
+@app.route('/predict', methods=['GET'])
+def predict():
+    data = request.get_json()
+    input_data = data.get('data', None)
+    if not input_data:
+        return {"error":"No se proporcionaron los datos necesarios para realizar la predicción. Ej:{'data': [[100, 100, 200]]}"}, 400
+    try:
+        predictions = []
+        for valor in input_data:
+            predictions.append(round(model.predict([valor])[0],2))
+        return {"prediction":predictions}, 200
+    except Exception as e:
+        return {'error':str(e)}, 500
+    
+@app.route('/retrain', methods=['POST'])
+def retrain():
+    try:
+        connection = sqlite3.connect('data/advertising.db')
+        cursor = connection.cursor()
+        query = '''SELECT * FROM campañas'''
+        result = cursor.execute(query).fetchall()
+        df = pd.DataFrame(result)
+        model.fit(df.iloc[:,:-1], df.iloc[:,-1])
+        with open('data/new_advertising_model.pkl', 'wb') as file:
+            pickle.dump(model, file)
+        return {'message': 'Modelo reentrenado correctamente.'}, 200
+    except Exception as e:
+        return {'error':str(e)}, 500
+
+if __name__ == '__main__':
+    app.run(port=5000, host='0.0.0.0')
